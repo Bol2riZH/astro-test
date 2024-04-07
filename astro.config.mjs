3a6512f963fc9816efbf9f4da19e676d6e7a44fa
Matthieu <reungoat.matthieu@gmail.com>
2024-04-07 15:34:33 +0200

    SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  image: {
-    domains: ['astro.build']
-  }
+  output: 'server',
 });
