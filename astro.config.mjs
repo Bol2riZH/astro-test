23bc13b8ed40c3a0931a558ed9db4dfd730248fb
Matthieu <reungoat.matthieu@gmail.com>
2024-04-07 15:35:48 +0200

    vercel deploiement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: 'server',
+  adapter: vercel()
 });
